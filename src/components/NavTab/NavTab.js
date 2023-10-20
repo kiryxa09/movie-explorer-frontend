b03d095a889a974db490b34b504bd0cd77c772ae
kirikland09 <baibakovkir@yandex.ru>
2023-10-20 22:33:15 +0300

    fix: fixed hrefs in navtab

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,13 +1,13 @@
 function NavTab() {
   return (
     <nav className="navtab">
-      <a className="navtab__link" href="#aboutproject">
+      <a className="navtab__link" href="#about-project">
         О проекте
       </a>
       <a className="navtab__link" href="#techs">
         Технологии
       </a>
-      <a className="navtab__link" href="#aboutme">
+      <a className="navtab__link" href="#about-me">
         Студент
       </a>
     </nav>
